2fc4fe9f934a1744d609b6c06eb6a121500d19de
jacquesmoraes <jacquesbmoraes@hotmail.com>
2023-10-20 18:57:20 -0300

    Project created

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "this will be products";
+        }
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return "this will be a product";
+        }
+
+    }
+}
